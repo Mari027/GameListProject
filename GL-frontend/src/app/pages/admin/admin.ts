7eb6062a58ca38727f0229cf45d3033ffc51efc3
Mari027 <mcfg.2003t@gmail.com>
2026-04-27 21:22:07 +0200

    27/04/2026 - 2 commit
    CSS completado

--- GL-frontend/src/app/pages/admin/admin.ts
+++ GL-frontend/src/app/pages/admin/admin.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ApiService } from '../../core/services/api-service';
 import { IUserResponse } from '../../core/interfaces/UserGames/IUserResponse';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -10,7 +11,7 @@ import { IUserResponse } from '../../core/interfaces/UserGames/IUserResponse';
 })
 export class Admin implements OnInit {
 
-  constructor(private apiService: ApiService, private cdr: ChangeDetectorRef) { }
+  constructor(private apiService: ApiService, private cdr: ChangeDetectorRef, private router: Router) { }
 
 
   users: IUserResponse[] = [];
@@ -36,5 +37,11 @@ export class Admin implements OnInit {
       next: () => alert("Usuario Eliminado Correctamente"),
       error: () => alert("No se ha podido eliminar el usuario")
     })
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    this.router.navigate(['/login']);
   }
 }
